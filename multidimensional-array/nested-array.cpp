0a637c3f9568394b2ddb91135bf568224db662c8
Chinedu Daniel <danielpreciouscinedu@gmail.com>
2026-09-13 04:41:49 +0100

    nested loop

---
+++ multidimensional-array/nested-array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+	using std::cout;
+	using std::endl;
+
+	int classroom[2][3] = {
+		{1, 2, 3},
+		{4, 5, 6}
+	};
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << classroom[i][j] << " ";
+		}
+
+		cout << endl;
+	}
+
+
+	return 0;
+}
